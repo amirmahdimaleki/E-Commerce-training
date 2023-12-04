af029cf0431b3d826a737e60dbfbb0ad7a87cfa9
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2023-12-04 20:41:31 +0330

    Fix the error later

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,20 @@
+const User = require('../models/User')
+const {StatusCodes} = require('http-status-codes')
+const {CustomError} = require('../middleware/error-handler')
+
+
 const getAllUsers = async(req, res) => {
-    res.send('all users')
+    // ^                                    removes pass from response
+    const users = await User.find({role : 'user'}).select('-password')
+    res.status(StatusCodes.OK).json({ users })
 }
 
 const getSingleUser = async(req, res) => {
-    res.send('one users')
-
+    const user = await User.findOne({_id : req.params.id}).select('-password')
+    if(!user){
+        throw new CustomError.NotFoundError(`No user with the id of ${req.params.id} found`)
+    }
+    res.status(StatusCodes.OK).json({ user })
 }
 const showCurrentUser = async(req, res) => {
     res.send('current user')
