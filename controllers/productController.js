48991a07d2afb5af8806c7a89170bff28d758c45
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2023-12-25 21:24:40 +0330

    still the bug exists. postman of product controller to be fixed

--- controllers/productController.js
+++ controllers/productController.js
@@ -11,8 +11,7 @@ const createProduct = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
     const products = await Product.find({})
-
-    res.status(StatusCodes.OK).json({ products })
+    res.status(StatusCodes.OK).json({ products, count: products.length })
 }
 
 const getSingleProduct = async (req, res) => {
@@ -26,12 +25,26 @@ const getSingleProduct = async (req, res) => {
 }
 
 const updateProduct = async (req, res) => {
-    res.send('updateProduct')
+    // just destructuring the id
+    const {id:ProductId} = req.params
+    const product = await Product.findOneAndUpdate({_id: ProductId}, req.body, {new: true, runValidators : true})
+
+    if(!product){
+        throw new CustomError(`no product with the id of ${req.params.id} found`)
+    }
+    res.status(StatusCodes.OK).json({product})
 }
 
 const deleteProduct = async (req, res) => {
-    res.send('deleteProduct')
+    const {id:ProductId} = req.params
+    const product = await Product.findOne({_id: ProductId})
 
+    if(!product){
+        throw new CustomError(`no product with the id of ${req.params.id} found`)
+    }
+
+    await product.remove()
+    res.status(StatusCodes.OK).json({ message : 'product removed successfully' })
 }
 
 const uploadImage = async (req, res) => {
